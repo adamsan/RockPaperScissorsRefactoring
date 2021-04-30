3def7d4fe0bd8a83d00aa2469cb32ff96bed315a
Adam Szentirmai-Schon <adamsan3@gmail.com>
2021-04-30 18:24:10 +0200

    Try to create some tests!

--- src/test/java/hu/adamsan/ApplicationTest.java
+++ src/test/java/hu/adamsan/ApplicationTest.java
@@ -1,12 +1,24 @@
 package hu.adamsan;
 
 import org.junit.jupiter.api.Test;
+import java.util.Random;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ApplicationTest {
 
     @Test
-    void main() {
+    void testMain() {
+        Scanner scanner = mock(Scanner.class); // Mockito cannot mock this, because it's a final class!
+        when(scanner.nextLine()).thenReturn("rock");
+
+        Random random = mock(Random.class);
+        when(random.nextInt(anyInt())).thenReturn(10).thenThrow(new RuntimeException("Random.nextInt() called twice"));
+
+        new Application.Game(scanner, System.out, random).invoke();
     }
 }
