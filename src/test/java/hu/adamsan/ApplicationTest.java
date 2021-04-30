956f6d2b45b0e46dcf054830fc2c03f081a77dfc
Adam Szentirmai-Schon <adamsan3@gmail.com>
2021-04-30 18:58:54 +0200

    add tests for another two cases of a tie

--- src/test/java/hu/adamsan/ApplicationTest.java
+++ src/test/java/hu/adamsan/ApplicationTest.java
@@ -28,4 +28,37 @@ class ApplicationTest {
         assertEquals(expected, out.getContent());
     }
 
+    @Test
+    void paperTiesWithPaper() {
+        MyScanner scanner = mock(MyScanner.class);
+        when(scanner.nextLine()).thenReturn("paper");
+
+        Random random = mock(Random.class);
+        when(random.nextInt(anyInt())).thenReturn(50).thenThrow(new RuntimeException("Random.nextInt() called twice"));
+
+        var out = new RememberingPrintStream(System.out);
+
+        new Application.Game(scanner, out, random).invoke();
+        String expected = "Do you pick rock, paper, or scissors?\n" +
+                "The computer chose: paper\n" +
+                "It's a tie!";
+        assertEquals(expected, out.getContent());
+    }
+
+    @Test
+    void scissorsTiesWithScissors() {
+        MyScanner scanner = mock(MyScanner.class);
+        when(scanner.nextLine()).thenReturn("scissors");
+
+        Random random = mock(Random.class);
+        when(random.nextInt(anyInt())).thenReturn(90).thenThrow(new RuntimeException("Random.nextInt() called twice"));
+
+        var out = new RememberingPrintStream(System.out);
+
+        new Application.Game(scanner, out, random).invoke();
+        String expected = "Do you pick rock, paper, or scissors?\n" +
+                "The computer chose: scissors\n" +
+                "It's a tie!";
+        assertEquals(expected, out.getContent());
+    }
 }
